faae64853c934a39b6965900837de4a3559c171e
Bhuvan Pancholi <bhuvan.pancholi94@gmail.com>
2024-03-29 21:37:33 +0100

    Pattern : Bhuvan

--- src/ComplexPattern.java
+++ src/ComplexPattern.java
@@ -7,7 +7,7 @@ public class ComplexPattern {
 
             for(j=0 ; j<n ; j++){
 
-                if (j == 0 || i == (n-1)/2 || i==j/2 || j==j/2)  {
+                if (i == 0 && j<=4 ||i == n-1 && j <= 4 || i == (n-1)/2 || j==j/2 || j-i == (n-1)/2 || j+i == 3*(n-1)/2) {
                     System.out.print("*");
                 } else{
                     System.out.print(" ");
@@ -40,7 +40,7 @@ public class ComplexPattern {
 
             for(j=0 ; j<n ; j++){
 
-                if ( j==0 || j == n-1 || (i-j == n/2) || i + j == n + n / 2 - 1) {
+                if (j==0 && i<= n-5 || j == n-1 && i <= n-5|| i-j==(n-1)/2 || i+j == 3 * (n-1)/2) {
                     System.out.print("*");
                 } else{
                     System.out.print(" ");
@@ -50,7 +50,7 @@ public class ComplexPattern {
 
             for(j=0 ; j<n ; j++){
 
-                if (i == 0 ||j==0 || i == (n-1)/2 || j == n-1) {
+                if (j==0 && i >=5 || i == (n-1)/2 || j == n-1 && i >=5 || i+j==(n-1)/2 || j-i == (n-1)/2) {
                     System.out.print("*");
                 } else{
                     System.out.print(" ");
@@ -61,7 +61,7 @@ public class ComplexPattern {
 
             for(j=0 ; j<n ; j++){
 
-                if (j==0 || j == n-1 || i==j) {
+                if (j==0 || j == n-1 || i == j) {
                     System.out.print("*");
                 } else{
                     System.out.print(" ");
