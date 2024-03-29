a1a88608d6c527f63e4863b6193a061b6adf2331
Bhuvan Pancholi <bhuvan.pancholi94@gmail.com>
2024-03-29 20:22:05 +0100

    Java_24032024

--- src/ComplexPattern.java
+++ src/ComplexPattern.java
@@ -1,2 +1,75 @@
-package PACKAGE_NAME;public class ComplexPattern {
+public class ComplexPattern {
+    public static void main(String[] args){
+        int i, j;
+        int n = 9;
+
+        for (i=0 ; i<n ; i++ ){
+
+            for(j=0 ; j<n ; j++){
+
+                if (j == 0 || i == (n-1)/2 || i==j/2 || j==j/2)  {
+                    System.out.print("*");
+                } else{
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.print("    ");
+
+            for(j=0 ; j<n ; j++){
+
+                if (j==0 || i==(n-1)/2 || j==(n-1)) {
+                    System.out.print("*");
+                } else{
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.print("    ");
+
+            for(j=0 ; j<n ; j++){
+
+                if (j==0 || i == n-1 || j == n-1) {
+                    System.out.print("*");
+                } else{
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.print("       ");
+
+            for(j=0 ; j<n ; j++){
+
+                if ( j==0 || j == n-1 || (i-j == n/2) || i + j == n + n / 2 - 1) {
+                    System.out.print("*");
+                } else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.print("       ");
+
+            for(j=0 ; j<n ; j++){
+
+                if (i == 0 ||j==0 || i == (n-1)/2 || j == n-1) {
+                    System.out.print("*");
+                } else{
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.print("       ");
+
+            for(j=0 ; j<n ; j++){
+
+                if (j==0 || j == n-1 || i==j) {
+                    System.out.print("*");
+                } else{
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+
+        }
+    }
 }
--- src/PatternProgramming.java
+++ src/PatternProgramming.java
@@ -1,2 +1,57 @@
-package PACKAGE_NAME;public class PatternProgramming {
+public class PatternProgramming {
+    public static void main(String[] args) {
+        int i, j;
+        int n = 9;
+
+        for (i = 0; i < n; i++) {
+
+            for (j = 0; j < n; j++) {
+
+                if (j == 0 || i - j == (n - 1) / 2 || i + j == (n - 1) / 2) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.print("    ");
+
+            for (j = 0; j < n; j++) {
+
+                if (i == 0 || j == 0 || i == n - 1 || j == n - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("         ");
+        }
+
+        System.out.println("         ");
+
+
+        for (i = 0; i < n; i++) {
+
+            for (j = 0; j < n; j++) {
+
+                if (j == 0 || i - j == (n - 1) / 2 || i + j == (n - 1) / 2) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.print("    ");
+
+            for (j = 0; j < n; j++) {
+
+                if (i == 0 || j == 0 || i == n - 1 || j == n - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
